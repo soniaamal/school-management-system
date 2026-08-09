10b2ab5dd159a2d73576f1017aeb95e258bbb058
soniaamal <soniaamal455@gmail.com>
2026-08-09 15:14:41 +0500

    Add recent activity to dashboard

--- frontend/school-management-ui/src/pages/Dashboard.jsx
+++ frontend/school-management-ui/src/pages/Dashboard.jsx
@@ -31,6 +31,29 @@ const state = [
 
     },
 ];
+const activities = [
+    {
+        icon: "👨‍🎓",
+        message: "Ahmed joined Class 8",
+        time: "10 min ago",
+    },
+    {
+        icon: "💰",
+        message: " Fees payment recived",
+        time: " 30 mint ago",
+
+    },
+    {
+        icon: "👩‍🏫",
+        message: "New Teacher added",
+        time: " 1 hour ago",
+    },
+    {
+        icon: "📋",
+        message: "Attendance marked for Class 7",
+        time: "2 hours ago",
+    },
+];
 
 function Dashboard() {
     return (
@@ -46,7 +69,38 @@ function Dashboard() {
                             icon={stat.icon}
                       />
                     </div>
-              ))}
+
+                    
+                ))}
+                                                
+                <div className="card shadow-sm border-0 mt-4">
+                    <div className="card-body">
+                        <h5 className="fw-bold mb-4">Recent Activity</h5>
+
+                        {activities.map((activity, index) => (
+                            <div
+                                key={index}
+                                className="d-flex align-item-center vorder-bottom py-3"
+                            >
+                                <div className="fs-4 me-3">
+                                    {activity.icon}
+                                </div>
+                                
+                                <div className="flex-grow-1">
+                                    <div className="fw-semibold">
+                                        {activity.message}
+                                    </div>
+
+                                    <small className="text-muted">
+                                        {activity.time}
+                                    </small>
+                                    
+                                </div>
+                            </div>        
+                        ))}
+
+                    </div>
+                </div>                 
             </div>
         </div>
 
