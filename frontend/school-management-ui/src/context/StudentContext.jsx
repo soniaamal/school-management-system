090b231f6003801b48616ea2c211abb793c9c8d6
soniaamal <soniaamal455@gmail.com>
2026-08-14 12:09:27 +0500

    Presist students with local storage

--- frontend/school-management-ui/src/context/StudentContext.jsx
+++ frontend/school-management-ui/src/context/StudentContext.jsx
@@ -1,28 +1,40 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 
 export const StudentContext = createContext();
 
 export const StudentProvider = ({ children }) => {
-      const [students, setStudents] = useState([
-    {
-      id: "ST001",
-      name: "Ahmed",
-      className: "8",
-      gender: "Male",
-    },
-    {
-      id: "ST002",
-      name: "Sara",
-      className: "7",
-      gender: "Female",
-    },
-    {
-      id: "ST003",
-      name: "Ali",
-      className: "9",
-      gender: "Male",
-    },
-      ]);
+    const [students, setStudents] = useState(() => {
+        const savedStudents = localStorage.getItem("students");
+
+        if (savedStudents) {
+            return JSON.parse(savedStudents);
+        }
+
+        return [
+            {
+                id: "ST001",
+                name: "Ahmed",
+                className: "8",
+                gender: "Male",
+            },
+            {
+                id: "ST002",
+                name: "Sara",
+                className: "7",
+                gender: "Female",
+            },
+            {
+                id: "ST003",
+                name: "Ali",
+                className: "9",
+                gender: "Male",
+            },
+        ];
+    });
+
+useEffect(() => {
+    localStorage.setItem("students", JSON.stringify(students));
+}, [students]);
       return (
     <StudentContext.Provider value={{ students, setStudents }}>
       {children}
@@ -30,7 +42,8 @@ export const StudentProvider = ({ children }) => {
   );
 };
 export const useStudents = () => {
-    return useContext(StudentContext);
+    const context = useContext(StudentContext);
+
      if (!context) {
         throw new Error(
             "useStudents must be used inside StudentProvider"
